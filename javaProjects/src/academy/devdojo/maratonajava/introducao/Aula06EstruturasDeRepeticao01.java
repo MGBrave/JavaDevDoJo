75692567908829373f54ee59e5635a5ca8f2bf3f
Mgeraldo <mggeraldo@hotmail.com>
2021-10-01 23:33:14 -0300

    while, do while, for

--- javaProjects/src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao01.java
+++ javaProjects/src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao01.java
@@ -2,6 +2,60 @@ package academy.devdojo.maratonajava.introducao;
 //Aula 28/286
 public class Aula06EstruturasDeRepeticao01 {
     public static void main(String[] args) {
+
+        int count = 0;
+        while (count < 10) {
+            System.out.println(++count);
+        }
+
+        count = 0;
+        do {
+            System.out.println("dentro do do-while "+ ++count);
+        } while (count < 10);
+    /*
+        for(count=0; count < 10; count++){
+            System.out.println("For " +count);
+            */
+    for(int i = 0; i < 10; i++){
+        System.out.println("For " +i);
+
+    }
+  }
+}
+/*
+    public static void main(String[] args) {
+        //while, do while, for
+        int count = 0;
+        while (count < 10){
+            System.out.println(++count);
+        }
+        count = 0;
+        do{
+            System.out.println("dentro do do-while "+ ++count);
+            /*System.out.println("dentro do do-while " +count);
+            count++;
+
+        }while (count < 10);
+    }
+}
+/*
+public class Aula06EstruturasDeRepeticao01 {
+
+    public static void main(String[] args) {
+        //while, do while, for
+        int count = 12;
+        while (count < 10){
+            System.out.println(++count);
+        }
+        do {
+            System.out.println("dentro do do-while");
+        }while (count < 10);
+    }
+}
+//
+public class Aula06EstruturasDeRepeticao01 {
+
+    public static void main(String[] args) {
         //while, do while, for
         int count = 0;
         while (count < 10){
@@ -9,7 +63,7 @@ public class Aula06EstruturasDeRepeticao01 {
             /*System.out.println(count);
            //count = count + 1;(pode ser desta maneira)
             //count += 2;(pode ser desta maneira)
-            count ++;*/
+            count ++;
         }
     }
-}
+}*/
