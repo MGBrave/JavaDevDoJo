fc14b2d97929278ba38127f04947555121ffaf6c
Mgeraldo <mggeraldo@hotmail.com>
2021-10-16 10:43:53 -0300

    Aulas 8: 01 02 02a 02b 02c 02d

--- javaProjects/src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais02b.java
+++ javaProjects/src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais02b.java
@@ -1,2 +1,19 @@
-package academy.devdojo.maratonajava.introducao;public class Aula08ArraysMultidimensionais02b {
+package academy.devdojo.maratonajava.introducao;
+//o exercício anterior pode ser feito desta forma
+public class Aula08ArraysMultidimensionais02b {
+    public static void main(String[] args) {
+        int[][] arrayInt = new int [3][];
+
+        arrayInt[0] = new int[2];
+        arrayInt[1] = new int[]{1,2,3};
+        arrayInt[2] = new int[] {1,2,3,4,5,6};
+
+        for(int[] arrayBase: arrayInt){
+            System.out.println("\n---------");
+            for(int num: arrayBase){
+                System.out.print(num + " ");
+            }
+        }
+    }
 }
+
