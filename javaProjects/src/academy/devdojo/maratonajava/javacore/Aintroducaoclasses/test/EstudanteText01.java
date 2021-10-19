f5586cda58d77589d63234735e70a288ae1dabcb
Mgeraldo <mggeraldo@hotmail.com>
2021-10-19 16:28:48 -0300

    aula 41/286

--- javaProjects/src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/EstudanteText01.java
+++ javaProjects/src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/EstudanteText01.java
@@ -1,6 +1,6 @@
-package academy.devdojo.maratonajava.javacore.introducaoclasses.test;
-
-import academy.devdojo.maratonajava.javacore.introducaoclasses.dominio.Estudante;
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+//aula 40/286
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Estudante;
 
 public class EstudanteText01 {
     public static void main(String[] args) {
