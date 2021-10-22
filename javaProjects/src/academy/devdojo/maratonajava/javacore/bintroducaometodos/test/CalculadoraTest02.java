16e52c2a9fb1c452f62240b44478b2547a5b2450
Mgeraldo <mggeraldo@hotmail.com>
2021-10-22 14:46:05 -0300

    Metodo calculadora, teste1 e teste2

---
+++ javaProjects/src/academy/devdojo/maratonajava/javacore/bintroducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.bintroducaometodos.test;
+import academy.devdojo.maratonajava.javacore.bintroducaometodos.dominio.Calculadora;
+//aula 46 de 286
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisNumeros(10, 20);
+
+    }
+}
