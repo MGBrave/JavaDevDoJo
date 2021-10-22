4825870f156ec3a20c6f298a775342129def0ffe
Mgeraldo <mggeraldo@hotmail.com>
2021-10-22 13:54:37 -0300

    javacore.Introduçãoclasses

---
+++ javaProjects/src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Professor.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio;
+//aula 42/286
+public class Professor {
+    public String nome;
+    public int idade;
+    public char sexo;
+}
+
+
+
