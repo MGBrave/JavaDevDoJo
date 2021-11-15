66933c70838a6479db4cd49c45d65dd894c2addd
Mgeraldo <mggeraldo@hotmail.com>
2021-11-15 14:30:45 -0300

    aulas

---
+++ javaProjects/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+//aula 46 de 286
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisNumeros(10, 20.5f);
+
+    }
+}
